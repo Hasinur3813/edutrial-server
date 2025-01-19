ff05715fe9fd7b7c36df604cfdfd20451a53d0a4
Hasinur <hasinur.rahman1332@gmail.com>
2025-01-19 10:13:14 +0600

    all classes page

--- route/adminRoute.js
+++ route/adminRoute.js
@@ -2,10 +2,12 @@ import express from "express";
 import verifyToken from "../middleware/verifyToken.js";
 import verifyAdmin from "../middleware/verifyAdmin.js";
 import { db } from "../config/db.js";
+import { ObjectId } from "mongodb";
 
 const adminRoute = express.Router();
 const teachersCollection = db.collection("teachers");
 const userCollection = db.collection("users");
+const classCollection = db.collection("classes");
 
 adminRoute.get(
   "/teachers",
@@ -187,5 +189,113 @@ adminRoute.get("/users", verifyToken, verifyAdmin, async (req, res, next) => {
     next(error);
   }
 });
+
+// get all the classes created by teachers
+
+adminRoute.get(
+  "/teachers-classes",
+  verifyToken,
+  verifyAdmin,
+  async (req, res, next) => {
+    try {
+      const result = await classCollection.find().toArray();
+      res.status(200).send({
+        error: false,
+        success: true,
+        message: "All the classes created by teachers",
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+// approve teacher classes
+adminRoute.patch(
+  "/approve-teacher-class/:id",
+  verifyToken,
+  verifyAdmin,
+  async (req, res, next) => {
+    const id = req.params?.id;
+    if (!id) {
+      return res.status(404).send({
+        message: "Required data not found",
+      });
+    }
+
+    const query = { _id: new ObjectId(id) };
+    const updateClass = {
+      $set: {
+        status: "accepted",
+      },
+    };
+    try {
+      const result = await classCollection.findOneAndUpdate(
+        query,
+        updateClass,
+        { returnDocument: "after" }
+      );
+      if (!result?.status) {
+        return res.status(404).send({
+          error: true,
+          success: false,
+          message: "It seems the class is not exist!",
+        });
+      }
+      res.send({
+        error: false,
+        success: true,
+        message: "The class has been accepted",
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+// reject teacher classes
+adminRoute.patch(
+  "/reject-teacher-class/:id",
+  verifyToken,
+  verifyAdmin,
+  async (req, res, next) => {
+    const id = req.params?.id;
+    if (!id) {
+      return res.status(404).send({
+        message: "Required data not found",
+      });
+    }
+
+    const query = { _id: new ObjectId(id) };
+    const updateClass = {
+      $set: {
+        status: "rejected",
+      },
+    };
+    try {
+      const result = await classCollection.findOneAndUpdate(
+        query,
+        updateClass,
+        { returnDocument: "after" }
+      );
+      if (!result?.status) {
+        return res.status(404).send({
+          error: true,
+          success: false,
+          message: "It seems the class is not exist!",
+        });
+      }
+      res.send({
+        error: false,
+        success: true,
+        message: "The class has been accepted",
+        data: result,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 export default adminRoute;
